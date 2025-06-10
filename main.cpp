7bc8d8d2f945fa9c6dfd360992d88b84e6941695
hwc20896 <hwc20896@gmail.com>
2025-06-10 21:47:27 +0800

    Corrected the context of main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
-#include "src/headers/mainwidget.hpp"
-
+#include "mainwidget.hpp"
 #include <QApplication>
+#include <QSqlDatabase>
+#include <QMessageBox>
 
-int main(
-    int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    Widget w;
-    w.show();
-    return a.exec();
+int main(int argc, char** argv) {
+    QApplication app(argc, argv);
+    QSqlDatabase database = QSqlDatabase::addDatabase("QSQLITE");
+    database.setDatabaseName("data.db");
+    if (database.open()) {
+        MainWidget widget(database);
+        widget.setFixedSize(1000,700);
+        widget.show();
+        return app.exec();
+    }
+    return 3;
 }
