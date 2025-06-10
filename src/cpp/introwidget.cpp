3760c7843f54d92478b7a42abd3595646c1c3aad
hwc20896 <hwc20896@gmail.com>
2025-06-10 21:48:24 +0800

    Did slight changes to Mute Switch

--- src/cpp/introwidget.cpp
+++ src/cpp/introwidget.cpp
@@ -4,9 +4,17 @@ IntroWidget::IntroWidget(QWidget* parent) : QWidget(parent), ui_(new Ui::IntroWi
     ui_->setupUi(this);
     isMuted = false;
 
+    muted.addFile(":/Drawables/drawables/mute.png");
+    unmuted.addFile(":/Drawables/drawables/unmute.png");
+
     //  Connections
     connect(ui_->ruleButton, &QPushButton::clicked, this, &IntroWidget::toRulePage);
     connect(ui_->startButton, &QPushButton::clicked, this, &IntroWidget::start);
+    connect(ui_->muteSwitch, &QPushButton::clicked, this, [this] {
+        isMuted = !isMuted;
+        ui_->muteSwitch->setIcon(isMuted? muted: unmuted);
+    });
+    ui_->muteSwitch->setIcon(unmuted);
 }
 
 IntroWidget::~IntroWidget() {
