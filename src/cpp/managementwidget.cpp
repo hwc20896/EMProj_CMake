354909310f7d95e563b75e95eed001ad0bab0b7d
hwc20896 <hwc20896@gmail.com>
2025-06-15 08:56:53 +0800

    Initialized management ui

--- src/cpp/managementwidget.cpp
+++ src/cpp/managementwidget.cpp
@@ -1,5 +1,45 @@
 #include "managementwidget.hpp"
+#include <random>
+#include <ranges>
 
-ManagementWidget::ManagementWidget(QWidget* parent) : QWidget(parent) {
+#define RANDOM_ALGORITHM std::mt19937(std::random_device()())
 
+ManagementWidget::ManagementWidget(const QSqlDatabase& database, const QJsonDocument& json, QWidget* parent)
+: QWidget(parent), ui_(new Ui::ManagementWidget), json_(json), database_(database) {
+    ui_->setupUi(this);
+    if (!database_.isOpen()) database_.open();
+    query_ = QSqlQuery(database);
+
+    totalQuantity = query_.value(0).toInt();
+    displayQuantity = json_["display_quantity"].toInt();
+
+    this->getQuestions();
+
+    for (auto&& [index, data] : std::views::enumerate(questions_)) {
+
+    }
+}
+
+ManagementWidget::~ManagementWidget() {
+    delete ui_;
+}
+
+void ManagementWidget::getQuestions() {
+    std::vector<int> idPool, sampled;
+    query_.exec("SELECT ID FROM QuestionData");
+    while (query_.next()) idPool.push_back(query_.value(0).toInt());
+
+    std::ranges::sample(idPool, std::back_inserter(sampled), displayQuantity, RANDOM_ALGORITHM);
+    for (const auto& id : sampled) {
+        query_.prepare("SELECT * FROM QuestionData WHERE ID = ?");
+        query_.addBindValue(id);
+        query_.exec();
+        query_.next();
+
+        const auto questionTitle = query_.value("QuestionTitle").toString();
+        const auto options = query_.value("Options").toString();
+        const auto correctOption = query_.value("CorrectOption").toInt();
+
+        questions_.emplace_back(questionTitle, options, correctOption);
+    }
 }
--- src/headers/managementwidget.hpp
+++ src/headers/managementwidget.hpp
@@ -1,13 +1,32 @@
 #pragma once
 #ifndef MANAGEMENTWIDGET_HPP
 #define MANAGEMENTWIDGET_HPP
-#include <QWidget>
 #include "templatewidget.hpp"
+#include <QSqlQuery>
+#include <QJsonDocument>
+
+#include "ui_managementwidget.h"
 
 class ManagementWidget final : public QWidget {
     Q_OBJECT
     public:
-        explicit ManagementWidget(QWidget* parent = nullptr);
+        explicit ManagementWidget(const QSqlDatabase& database, const QJsonDocument& json, QWidget* parent = nullptr);
+        ~ManagementWidget() override;
+    private:
+        Ui::ManagementWidget* ui_;
+
+        //  Query engine
+        QJsonDocument json_;
+        QSqlDatabase database_;
+        QSqlQuery query_;
+
+        //  Question Data
+        void getQuestions();
+        std::vector<QuestionData> questions_;
+        std::vector<QuestionWidget> pages_;
+
+        //  Configs
+        int totalQuantity, displayQuantity;
 };
 
 #endif
