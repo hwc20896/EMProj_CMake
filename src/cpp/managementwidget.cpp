e1bdf2c427f5321c72856fdb72f4e52887148d54
hwc20896 <hwc20896@gmail.com>
2025-06-18 21:27:55 +0800

    Finished the core of the question game

--- src/cpp/managementwidget.cpp
+++ src/cpp/managementwidget.cpp
@@ -1,25 +1,84 @@
 #include "managementwidget.hpp"
+
 #include <random>
 #include <ranges>
 
-ManagementWidget::ManagementWidget(const QSqlDatabase& database, const QJsonDocument& json, QWidget* parent)
+ManagementWidget::ManagementWidget(const QSqlDatabase& database, const QJsonDocument& json, const bool currentMuted, QWidget* parent)
 : QWidget(parent), ui_(new Ui::ManagementWidget), json_(json), database_(database) {
     ui_->setupUi(this);
+    stackLayout_ = new QStackedLayout(this);
+
+    //  Get Question Data
     if (!database_.isOpen()) database_.open();
     query_ = QSqlQuery(database);
-
     totalQuantity = query_.value(0).toInt();
     displayQuantity = json_["display_quantity"].toInt();
-
     this->getQuestions();
 
+    pageFinished = std::vector(questions_.size(), false);
     for (auto&& [index, data] : std::views::enumerate(questions_)) {
+        const auto widget = new QuestionWidget(std::move(data), index + 1);
+        stackLayout_->addWidget(widget);
 
+        //  Time recorder for total time and time per question use
+        connect(widget, &QuestionWidget::timeTap, this, [this] {
+            end_ = std::chrono::high_resolution_clock::now();
+            timeStamps.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(end_ - start_).count());
+        });
+
+        //  Score recorder
+        connect(widget, &QuestionWidget::score, this, [this, index](const bool isCorrect) {
+            pageFinished[index] = true;
+            isCorrect? correctCount++ : incorrectCount++;
+            ui_->nextQuestion->setEnabled(true);
+            this->updatePages();
+        });
     }
+
+    //   Navigations
+    stackLayout_->setCurrentIndex(0);
+    this->updatePages();
+    connect(stackLayout_, &QStackedLayout::currentChanged, this, [this] (const int index) {
+        ui_->prevQuestion->setVisible(index != 0);
+        ui_->nextQuestion->setText(index < displayQuantity-1? "下一頁 →": "完成");
+        ui_->nextQuestion->setEnabled(pageFinished[index]);
+    });
+    connect(ui_->prevQuestion, &QPushButton::clicked, this, [this] {stackLayout_->setCurrentIndex(stackLayout_->currentIndex() - 1);});
+    connect(ui_->nextQuestion, &QPushButton::clicked, this, [this] {
+        if (const auto current = stackLayout_->currentIndex(); current < displayQuantity-1 ) {
+            stackLayout_->setCurrentIndex(current + 1);
+            start_ = std::chrono::high_resolution_clock::now();
+        }
+        else emit finish(correctCount, muteSwitch_->getMutedState(), timeStamps);
+    });
+    ui_->prevQuestion->hide();
+
+    //  BGM
+    audioOutput_ = new QAudioOutput(this);
+    audioOutput_->setVolume(0.15f);
+    audioOutput_->setMuted(currentMuted);
+
+    player_ = new QMediaPlayer(this);
+    player_->setAudioOutput(audioOutput_);
+    player_->setSource({"qrc:/BGM/sounds/OMFG_Pizza.mp3"});
+    player_->setLoops(QMediaPlayer::Infinite);
+
+    //  Mute switch
+    currentMuted_ = currentMuted;
+    muteSwitch_ = new MuteSwitch({50,50}, currentMuted_);
+    muteSwitch_->setGeometry(570,10,60,60);
+    connect(muteSwitch_, &MuteSwitch::mutedStateChanged, this, [this] (const bool currentState) {
+        currentMuted_ = currentState;
+        audioOutput_->setMuted(currentMuted_);
+    });
+
+    //  Question layout
+    ui_->optionWidget->setLayout(stackLayout_);
 }
 
 ManagementWidget::~ManagementWidget() {
     delete ui_;
+    delete muteSwitch_;
 }
 
 void ManagementWidget::getQuestions() {
@@ -40,4 +99,28 @@ void ManagementWidget::getQuestions() {
 
         questions_.emplace_back(questionTitle, options, correctOption);
     }
+}
+
+template<class T> requires std::is_arithmetic_v<T>
+QString ManagementWidget::timeDisplay(const T duration) {
+    if (duration > 60000LL) {
+        const double remain = remainder(duration,60.0);
+        return QString("%1分%2秒").arg(QString::number(std::floor(duration/60000LL)), QString::number(remain<0?remain+60:remain,'g',3));
+    }
+    if (duration == 60000LL) return "1分鐘";
+    if (duration >= 0) return QString("%1秒").arg(QString::number(duration/1000,'g',3));
+    throw std::range_error("Invalid duration");
+}
+
+void ManagementWidget::updatePages() const {
+    setScore(correctCount, incorrectCount);
+    setProgress(correctCount + incorrectCount, totalQuantity);
+}
+
+void ManagementWidget::setScore(const int correct, const int incorrect) const {
+    ui_->corrCount->setText(QString(COLOR(錯誤數 %1,"#ff0000")" | " COLOR(%2 正確數,"#00dd12")).arg(QString::number(incorrect), QString::number(correct)));
+}
+
+void ManagementWidget::setProgress(const int current, const int total) const {
+    ui_->progress->setText(QString("進度：%1 / %2 - %3%").arg(QString::number(current), QString::number(total), QString::number(current*100.0/total,'g',1)));
 }
--- src/headers/managementwidget.hpp
+++ src/headers/managementwidget.hpp
@@ -1,19 +1,36 @@
 #pragma once
 #ifndef MANAGEMENTWIDGET_HPP
 #define MANAGEMENTWIDGET_HPP
-#include "templatewidget.hpp"
+#include <chrono>
+#include <QAudioOutput>
+#include <QMediaPlayer>
 #include <QSqlQuery>
-#include <QJsonDocument>
+#include <QStackedLayout>
+#include <QTimer>
+#include <vector>
 
+#include "muteswitch.hpp"
+#include "templatewidget.hpp"
 #include "ui_managementwidget.h"
 
 class ManagementWidget final : public QWidget {
     Q_OBJECT
     public:
-        explicit ManagementWidget(const QSqlDatabase& database, const QJsonDocument& json, QWidget* parent = nullptr);
+        explicit ManagementWidget(const QSqlDatabase& database, const QJsonDocument& json, bool currentMuted, QWidget* parent = nullptr);
         ~ManagementWidget() override;
+
+        /**
+         * @brief Converts the duration (second) to time format.
+         * @tparam T Supports types that are arithmetic.
+         * @param duration The duration to be formatted.
+         * @return The formatted time.
+         */
+        template <class T> requires std::is_arithmetic_v<T>
+        static QString timeDisplay(T duration);
     private:
         Ui::ManagementWidget* ui_;
+        MuteSwitch* muteSwitch_;
+        QStackedLayout* stackLayout_;
 
         //  Query engine
         QJsonDocument json_;
@@ -24,9 +41,27 @@ class ManagementWidget final : public QWidget {
         void getQuestions();
         std::vector<QuestionData> questions_;
         std::vector<QuestionWidget> pages_;
+        int correctCount=0, incorrectCount=0;
+
+        //  Navigation use
+        std::vector<bool> pageFinished;
+        void updatePages() const;
+        void setScore(int correct, int incorrect) const;
+        void setProgress(int current, int total) const;
 
         //  Configs
         int totalQuantity, displayQuantity;
+
+        //  Timers
+        std::vector<int64_t> timeStamps;
+        std::chrono::time_point<std::chrono::system_clock> start_, end_;
+
+        //  Background music
+        bool currentMuted_ = false;
+        QAudioOutput* audioOutput_;
+        QMediaPlayer* player_;
+    signals:
+        void finish(int correctCount, bool currentMuted, const std::vector<int64_t>& timestamps);
 };
 
 #endif
