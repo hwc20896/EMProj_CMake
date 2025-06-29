32d00e4afa97a22271314e38bf733b2eb0e88c22
hwc20896 <hwc20896@gmail.com>
2025-06-29 18:31:09 +0800

    Fixed json config "toggle_default_mute_background" not working properly

--- src/cpp/mainwidget.cpp
+++ src/cpp/mainwidget.cpp
@@ -6,25 +6,29 @@ MainWidget::MainWidget(const QSqlDatabase& database, QWidget* parent) : QStacked
     query_.next();
     totalQuantity = query_.value(0).toInt();
 
-    intro_ = new IntroWidget;
-    rule_ = new RuleWidget;
     displayQuantity = 0;
 
     management_ = nullptr;
+
+    bool defaultMuted = false, jsonFileAccessed = false;
 
     if (QFile file("config.json");
         file.open(QFile::ReadOnly | QFile::Text)
     ) {
         json_ = QJsonDocument::fromJson(file.readAll());
         displayQuantity = json_["display_quantity"].toInt();
+        defaultMuted = json_["toggle_default_mute_background"].toBool();
+        jsonFileAccessed = true;
     }
-    else {
-        intro_->blockStart();
-    }
+
+    intro_ = new IntroWidget(defaultMuted);
+    rule_ = new RuleWidget;
 
     this->addWidget(intro_);
     this->addWidget(rule_);
     this->setCurrentIndex(0);
+
+    if (!jsonFileAccessed) intro_->blockStart();
 
     connect(intro_, &IntroWidget::toRulePage, this, [this] {
         this->setCurrentIndex(1);
