3727b99ae2ce3c8647a1f2bec009ebdf6278436b
hwc20896 laptop <hwc20896@gmail.com>
2025-08-08 20:59:16 +0800

    Fixed combo box not displaying properly

--- src/headers/elements/gamemodechooser.hpp
+++ src/headers/elements/gamemodechooser.hpp
@@ -6,28 +6,34 @@
 class GamemodeChooser final : public QComboBox {
     Q_OBJECT
     public:
-        explicit GamemodeChooser(int currentMode, QWidget* parent = nullptr);
+        explicit GamemodeChooser(int currentMode = 0, QWidget* parent = nullptr);
 
         [[nodiscard]] int getCurrentMode() const;
     private:
         QStringList gamemodeList_;
 
         // Stylesheet
-        const char* STYLESHEET = R"(
-            QComboBox {
-                background-color: #f0f0f0;
-                border: 1px solid #ccc;
-                border-radius: 4px;
-                padding: 5px;
-            }
-            QComboBox::drop-down {
-                subcontrol-origin: padding;
-                subcontrol-position: top right;
-                width: 20px;
-            }
-            QComboBox::down-arrow {
-                image: url(:/Drawables/drawables/down_arrow.png);
-            }
+        static constexpr auto STYLESHEET = R"(
+QComboBox{
+    border: 1px solid black;
+    border-radius: 3px;
+    padding-left: 8px;
+    background-color: #f0f0f0;
+}
+
+QComboBox:hover{
+    background-color: #e3e3e3;
+}
+
+QComboBox::down-arrow{
+    image: url(:/Drawables/drawables/arrow_down.png);
+    width: 10;
+    height: 10;
+}
+
+QComboBox::drop-down{
+    border: none;
+}
         )";
 };
 
