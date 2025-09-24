fc3e0832791e5a539e377b034fb9f14a827c46b1
hwc20896 <hwc20896@gmail.com>
2025-09-24 18:50:13 +0800

    Reverted the changes about markdowns

--- src/headers/utilities/database.hpp
+++ src/headers/utilities/database.hpp
@@ -11,9 +11,21 @@ namespace Data {
             explicit Database() = default;
             ~Database() = default;
 
+            /**
+             * @brief Get the number of questions in the database for a specific gamemode.
+             * @param gamemode The gamemode to get the question count for. 0 for all, 1 for 基本法, 2 for 憲法.
+             * @return The number of questions in the database for the specified gamemode.
+             */
             [[nodiscard]]
             int getQuestionCount(int gamemode = 0);
 
+            /**
+             * @brief Get a list of questions from the database.
+             * @param gamemode The gamemode to get questions for. 0 for all, 1 for 基本法, 2 for 憲法.
+             * @param count The number of questions to get. Default is 10.
+             * @return A vector of QuestionData objects.
+             */
+            [[nodiscard]]
             std::vector<QuestionData> getQuestions(int gamemode, int count);
         private:
             sqlite::database db_{"data.db"};
